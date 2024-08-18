391ba7107f2f48d006f7af0f24d07b0b3c13d6a1
vitorsoligo <127339888+vitorsoligo@users.noreply.github.com>
2024-08-17 22:11:47 -0300

    Add files via upload

---
+++ Portfolio Soligo/Portfolio Soligo/script.js
@@ -0,0 +1,24 @@
+
+function mostrarBotoesPPE() {
+    var botoesPPE = document.querySelector('.ppe-section .botoes-ppe');
+    if (botoesPPE.style.display === 'none') {
+        botoesPPE.style.display = 'block';
+    } else {
+        botoesPPE.style.display = 'none';
+    }
+}
+  
+  function mostrarPrimeiroTrimestre() {
+    alert("Primeiro Trimestre selecionado!");
+    // Adicione aqui o código para exibir o conteúdo do primeiro trimestre
+  }
+  
+  function mostrarSegundoTrimestre() {
+    alert("Segundo Trimestre selecionado!");
+    // Adicione aqui o código para exibir o conteúdo do segundo trimestre
+  }
+  
+  function mostrarTerceiroTrimestre() {
+    alert("Terceiro Trimestre selecionado!");
+    // Adicione aqui o código para exibir o conteúdo do terceiro trimestre
+  }
